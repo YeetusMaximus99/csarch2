9be5e134c2d4ef54504d0a647fb4828a706df43d
Jan Canicon <Jan_Canicon@dlsu.edu.ph>
2022-02-09 17:59:48 +0800

    Added JQuery and copy button

--- public/script.js
+++ public/script.js
@@ -1,3 +1,22 @@
+$(document).ready(function(){
+    $('#copyButton').on('click', copyToClipboard);
+    
+    console.log(unsigned_binary(1)); // 1
+    console.log(unsigned_binary(-1)); // 11111111111111111111111111111111
+    console.log(unsigned_binary(256)); // 100000000
+    console.log(unsigned_binary(-256)); // 11111111111111111111111100000000
+    console.log(unpacked_bcd(324))
+});
+
+function copyToClipboard() {
+    var text = $('#outputValue').val();
+
+    navigator.clipboard.writeText(text);
+    
+    /* Alert the copied text */
+    alert("Copied the text: " + text);
+  }
+
 /*def unpacked_bcd(decimal):
     bcd = ''
     for unit in decimal:
@@ -7,28 +26,21 @@
         bcd = bcd + temp
     return bcd
 */
-    
-    function unpacked_bcd(decimal){
-        var bcd = '';
-        var output = [];
-        var sNumber = decimal.toString();
-        for(var i =0, len = sNumber.length;i < len; i+=1){
-            temp = unsigned_binary(parseInt(sNumber.charAt(i)))
-            while(temp.length < 8){
-                temp = '0' + temp
-            }
-            bcd = bcd + temp
-        }
-        return bcd
-    }
-    
-    function unsigned_binary(dec) {
-        return (dec >>> 0).toString(2);
-      }
-      
-      console.log(unsigned_binary(1)); // 1
-      console.log(unsigned_binary(-1)); // 11111111111111111111111111111111
-      console.log(unsigned_binary(256)); // 100000000
-      console.log(unsigned_binary(-256)); // 11111111111111111111111100000000
-      console.log(unpacked_bcd(324))
 
+function unpacked_bcd(decimal){
+    var bcd = '';
+    var output = [];
+    var sNumber = decimal.toString();
+    for(var i =0, len = sNumber.length;i < len; i+=1){
+        temp = unsigned_binary(parseInt(sNumber.charAt(i)))
+        while(temp.length < 8){
+            temp = '0' + temp
+        }
+        bcd = bcd + temp
+    }
+    return bcd
+}
+
+function unsigned_binary(dec) {
+    return (dec >>> 0).toString(2);
+}
