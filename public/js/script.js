bc9acad2d5340bbf3b9e9e6af463abc0632ed182
Kashkanov <aquinokarl17@gmail.com>
2022-02-11 21:11:04 +0800

    console logs for testing

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function() {
     // Convert input based on selected method
     $('#convertButton').on('click', function() {
         var inputElement = $('#input-value');
@@ -13,7 +13,7 @@ $(document).ready(function(){
         var expcont;
         var exp = 0;
         var significand;
-       
+
         $(outputElement).css('color', 'white');
 
         if (input == '') {
@@ -24,90 +24,93 @@ $(document).ready(function(){
         let pattern = /e/;
         let pattern1 = /E/;
         console.log(pattern.test(input.toString()));
-        if (pattern.test(input.toString())){
+        if (pattern.test(input.toString())) {
             var array = input.split('e');
             significand = array[0];
             console.log(significand);
             exp = parseInt(array[1]);
             console.log("exps1");
             console.log(exp);
-        }
-        
-        else if (pattern1.test(input.toString())){
+        } else if (pattern1.test(input.toString())) {
             var array = input.split('E');
             significand = parseInt(array[0]);
             console.log(array[0]);
             exp = parseInt(array[1]);
             console.log("exps2");
             console.log(exp);
-        }
-
-        else{
+        } else {
             significand = input;
         }
 
         signbit = get_sign_bit(input);
-        if(signbit == 1){
+        if (signbit == 1) {
             significand = significand.substring(1);
             significand = parseInt(significand);
-     
+
         }
 
         console.log(significand);
         var i;
         var array;
-        while(significand.toString().includes('.')){
-            if(i < 8){
-            significand = significand * 10;
-            console.log(significand);
-            exp = exp - 1;
-            i++;
-            }
-            else{
+        while (significand.toString().includes('.')) {
+            if (i < 8) {
+                significand = significand * 10;
+                console.log(significand);
+                exp = exp - 1;
+                i++;
+            } else {
                 significand = significand.toString();
-                array=significand.split('.');
+                array = significand.split('.');
                 significand = array[0];
                 console.log(array[1]);
                 significand = significand.concat(array[1]);
             }
         }
-        
-        
-        while(significand.toString().length < 7){
+
+
+        while (significand.toString().length < 7) {
             significand = significand * 10;
             exp = exp - 1;
         }
-        
+
         /*5.673459e6*/
         significand = significand.toString();
         tempMSD = significand[0];
         tempbcd = significand.substring(1);
 
-        switch(method) {
+        switch (method) {
             case "NR":
-                if(significand.length > 7){
+                if (significand.length > 7) {
                     $('#outputValue').val("Please Input a digit thats less than or equal to 7 digits");
-                }
-                else{
-                tempbcd = densely_fixer(tempbcd);
-                expcont = get_exponent_continuation(exp);
-                
-                combifield = combination_field(expcont,tempMSD);
-                expcont = expcont.substring(2);
-                $(outputElement).text(signbit + " " + combifield + " " + expcont + " " + tempbcd);
+                } else {
+                    console.log(tempbcd);
+                    console.log("sign = " + signbit); //<===
+                    tempbcd = densely_fixer(tempbcd);
+                    expcont = get_exponent_continuation(exp);
+                    console.log("e' = " + expcont); //<===
+                    combifield = combination_field(expcont, tempMSD);
+                    console.log("combi = " + combifield); //<===
+                    expcont = expcont.substring(2);
+                    console.log("exp cont. = " + expcont); //<===
+                    $(outputElement).text(signbit + " " + combifield + " " + expcont + " " + tempbcd);
                 }
                 break;
-                
+
             case "RTNE":
                 console.log('Option2');
                 newSignificand = round_to_nearest_even(significand);
                 tempMSD = newSignificand[0];
                 tempbcd = newSignificand.substring(1);
 
+                console.log(tempbcd);
+                console.log("sign = " + signbit); //<===
                 tempbcd = densely_fixer(tempbcd);
                 expcont = get_exponent_continuation(exp);
-                combifield = combination_field(expcont,tempMSD);
+                console.log("e' = " + expcont); //<===
+                combifield = combination_field(expcont, tempMSD);
+                console.log("combi = " + combifield); //<===
                 expcont = expcont.substring(2);
+                console.log("exp cont. = " + expcont); //<===
                 $(outputElement).text(signbit + " " + combifield + " " + expcont + " " + tempbcd);
                 break;
 
